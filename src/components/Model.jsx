cd1aaa7dfbccc02e41263411ada8133b7f9fbed7
Sharanspujari <sharanspujari142@gmail.com>
2024-05-15 16:51:59 +0530

    final commit

--- src/components/Model.jsx
+++ src/components/Model.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
-import AddUserForm from "./AddUserForm";
+import AddUserForm from "./AddStudentForm";
 
 const Model = ({ setOpen, open }) => {
 
